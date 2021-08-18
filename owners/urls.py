dba94426fddb610eb0e1e61f9d2a1a7a71f70576
hyun-soo choi <hyun-soochoi@hyun-sooui-MacBookAir.local>
2021-08-18 09:28:39 +0900

    Add : GET

--- owners/urls.py
+++ owners/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from owners.views import OwnerView, DogView
+from owners.views import OwnerView, DogView, OwnerListView, DogListView
 urlpatterns = [
     path('owner', OwnerView.as_view()),
     path('dog', DogView.as_view()),
+    path('ownerlist', OwnerListView.as_view()),
+    path('doglist', DogListView.as_view()),
 ]
--- owners/views.py
+++ owners/views.py
@@ -5,13 +5,14 @@ from django.views import View
 
 from owners.models import Dog, Owner
 # Create your views here.
-
+# 주인 post
 class OwnerView(View):
     def post(self, request):
         data = json.loads(request.body)
-        owner = Owner.objects.create(name=data['owner'], email=data['email'], age=data['o_age'], dog=dog)
+        owner = Owner.objects.create(name=data['owner'], email=data['email'], age=data['o_age'])
         return JsonResponse({'MESSAGE':'OWNER CREATED'}, status=200)
 
+# 강아지 post
 class DogView(View):
     def post(self, request):
         data = json.loads(request.body)
@@ -19,25 +20,34 @@ class DogView(View):
         dog = Dog.objects.create(name=data['dog'], age=data['d_age'],owner=owner)
         return JsonResponse({'MESSAGE':'Dog CREATED'}, status=200)
 
-
-
-
-
-
-
-    # def get(self, request):
-    #     owners = Owner.objects.all()
-    #     result=[]
-    #     for owner in owners:
-    #         result.append(
-    #             {
-    #                 "name" : owner.name,
-    #                 "age" : owner.age,
-    #                 "email" : owner.email,
-    #                 # "dog" : {
-    #                 #     "name" : owner.dog.dog,
-    #                 #     "age" : owner.dog.age 
-    #                 # }
-    #             }
-    #         )
-    #     return JsonResponse({'results':result}, status=200)
+# 주인 목록 get
+class OwnerListView(View):
+    def get(self, request):
+        owners = Owner.objects.all()
+        result=[]
+        for owner in owners:
+            dog = Dog.objects.filter(owner=owner)
+            result.append(
+                {
+                    "name" : owner.name,
+                    "age" : owner.age,
+                    "email" : owner.email,
+                    "dog" : list(dog.values())
+                }
+            )
+        return JsonResponse({'results':result}, status=200)
+        
+# 강아지 목록 get
+class DogListView(View):
+    def get(self, request):
+        dogs = Dog.objects.all()
+        result=[]
+        for dog in dogs:
+            result.append(
+                {
+                    "name" : dog.name,
+                    "age" : dog.age,
+                    "owner" : dog.owner.name
+                }
+            )
+        return JsonResponse({'results':result}, status=200)
